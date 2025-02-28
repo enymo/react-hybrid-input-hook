321d609f17c60636b381d311e0182101d677d692
Gideon Ritter <gideon.ritter@enymo.com>
2025-02-28 11:46:00 +0100

    Add error to hybrid input hook

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,28 @@ import React, { useCallback } from "react";
 import { RegisterOptions, useController, useForm, useFormContext } from "react-hook-form";
 import { NativeSyntheticEvent, TextInputFocusEventData } from "react-native";
 
-export default function useHybridInput<T, U extends React.FocusEvent | NativeSyntheticEvent<TextInputFocusEventData>>({name = "", externalOnChange, externalOnBlur, externalValue, defaultValue, options}: {
+export default function useHybridInput<T, U extends React.FocusEvent | NativeSyntheticEvent<TextInputFocusEventData>>({
+    name = "",
+    externalOnChange,
+    externalOnBlur,
+    externalValue,
+    externalError,
+    defaultValue,
+    options
+}: {
     name?: string,
     externalOnChange?: (value: T) => void | Promise<void>,
     externalOnBlur?: (e: U) => void,
     externalValue?: T,
+    externalError?: string,
     defaultValue?: T,
     options?: RegisterOptions
 }) {
     const form = useFormContext();
     const { control: fallbackControl } = useForm();
-    const {field: {onChange: internalOnChange, onBlur: internalOnBlur, value: internalValue}, fieldState: {error}} = useController({name, control: form?.control ?? fallbackControl, rules: options, defaultValue})
+    const {field: {onChange: internalOnChange, onBlur: internalOnBlur, value: internalValue}, fieldState: {error: internalError}} = useController({name, control: form?.control ?? fallbackControl, rules: options, defaultValue})
     const value: T = (form && name) ? internalValue : externalValue;
+    const error = (form && name) ? internalError?.message : externalError
 
     const onChange = useCallback((value: T) => {
         internalOnChange?.(value);
